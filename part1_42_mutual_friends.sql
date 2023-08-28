b6e1ab5a1b0998346103d39504c641897cbfd3b1
derek901128 <96558860+derek901128@users.noreply.github.com>
2023-08-28 23:54:31 +0800

    Update part1_42_mutual_friends.sql

--- part1_42_mutual_friends.sql
+++ part1_42_mutual_friends.sql
@@ -1,8 +1,10 @@
 with 
-base(
+base
+(
     friend_1,
     friend_2
-) as (
+) as 
+(
     select 'jason', 'mary' from dual union all
     select 'mike', 'mary' from dual union all
     select 'mike', 'jason' from dual union all
@@ -10,14 +12,16 @@ base(
     select 'john', 'mary' from dual union all
     select 'susan', 'mary' from dual
 ),
-add_rn as (
+add_rn as 
+(
     select 
     	row_number()over(order by 1) as rn,
     	base.*
     from
     	base
 ),
-mutual as (
+mutual as 
+(
     select 	
     	a.rn,
     	a.friend_1,
@@ -28,32 +32,23 @@ mutual as (
     	e.friend_2 as d
     from
     	add_rn a
-    left join
-    	add_rn b 
-    on
-    	a.friend_1 = b.friend_1
-    and 
-    	a.friend_2 <> b.friend_2
-    left join
-    	add_rn c
-    on
-    	a.friend_1 = c.friend_2
-    left join
-    	add_rn d
-    on
-    	a.friend_2 = d.friend_2
-    and 
-    	a.friend_1 <> d.friend_1
-    left join
-    	add_rn e
-    on
-    	a.friend_2 = e.friend_1
+	    left join add_rn b 
+		    on a.friend_1 = b.friend_1
+		    and a.friend_2 <> b.friend_2
+	    left join add_rn c
+		    on a.friend_1 = c.friend_2
+	    left join add_rn d
+		    on a.friend_2 = d.friend_2
+		    and a.friend_1 <> d.friend_1
+	    left join add_rn e
+		    on a.friend_2 = e.friend_1
 )
 select 
 	rn,
     friend_1,
     friend_2,
-    sum(
+    sum
+	(
     	case 
     		when a = c or a = d or b = c or b = d 
     		then 1 
