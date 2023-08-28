d30156766569c5e8bd067bd17d21657129b007be
derek901128 <96558860+derek901128@users.noreply.github.com>
2023-08-28 23:55:10 +0800

    Update part1_43_unbounded_preceding.sql

--- part1_43_unbounded_preceding.sql
+++ part1_43_unbounded_preceding.sql
@@ -1,9 +1,11 @@
 with 
-base (
+base 
+(
     oid, 
     cid,
     qty
-) as (
+) as 
+(
     select 1, 1001, 5 from dual union all
     select 2, 1001, 8 from dual union all
     select 3, 1001, 3 from dual union all
@@ -15,7 +17,7 @@ select
 	oid,
 	cid,
 	qty,
-	min(qty)over(partition by cid order by rownum rows between unbounded preceding and current row) as min_value
+	min(qty) over(partition by cid order by rownum rows between unbounded preceding and current row) as min_value
 from
 	base
 ;
