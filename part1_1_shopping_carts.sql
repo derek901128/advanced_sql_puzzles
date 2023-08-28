b1d43cf8bae888561b352561992c911beda4a35f
derek901128 <96558860+derek901128@users.noreply.github.com>
2023-08-28 23:50:28 +0800

    Update part1_1_shopping_carts.sql

--- part1_1_shopping_carts.sql
+++ part1_1_shopping_carts.sql
@@ -1,30 +1,33 @@
 with 
-cart_one(item) as (
+cart_one(item) as 
+(
     select 'sugar' from dual union all
     select 'bread' from dual union all
     select 'juice' from dual union all
     select 'soda' from dual union all
     select 'flour' from dual
 ),
-cart_two(item) as (
+cart_two(item) as 
+(
     select 'bread' from dual union all
     select 'sugar' from dual union all
     select 'butter' from dual union all
     select 'cheese' from dual union all
     select 'fruit' from dual
 ),
-solution(
+solution
+(
     item_cart_one,
     item_cart_two
-) as (
+) as 
+(
     select 
         a.item,
         b.item
     from 
         cart_one a
-    full outer join
-        cart_two b
-    on
-        a.item = b.item 
+        full outer join cart_two b
+            on a.item = b.item 
 )
-select * from solution;
+select * from solution
+;
